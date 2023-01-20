5383fbc885b763c385bc18d94ca8e928556bee03
Daniel Andarge <andargedaniel90@gmail.com>
2023-01-20 06:36:19 +0300

    main

---
+++ src/components/Main.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Post } from "./Post";
+import { Pagination } from "./Pagination";
+import { useFetchData } from "./useFetchData";
+import { PostContainer } from "./styles/PostContainer.styled";
+
+export const Main = () => {
+  const { data } = useFetchData();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(8);
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(data.length / recordsPerPage);
+
+return (
+    <React.Fragment>
+
+      <PostContainer>
+        {currentRecords.map((post, index) => (
+          <Post post={post} key={index} />
+        ))}
+      </PostContainer>
+
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+
+   </React.Fragment>
+  );
+};
